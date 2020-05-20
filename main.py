03fc520b0237d2eb0de29e6b8b9b9d29da1b4f79
mhollenbeck <moritzhollenbeck@gmx.de>
2020-05-20 22:23:49 +0200

    - added functions to assemble search disctionary

--- main.py
+++ main.py
@@ -20,9 +20,91 @@ longSearch = False
 matchFound = False
 foundExact = 0
 foundSynonym = 0
+overwrittenEntries = 0
+
+def makeEntry(rest, indentifier):
+  if len(rest) == 0:
+      return rest
+  elif len(rest) == 1:
+      rest = {rest[0]: indentifier}
+      return rest
+  else:
+      first_value = rest[0]
+      rest = {first_value: makeEntry(rest[1:], indentifier)}
+      return rest
 
 
-#zu erklärung : auth=("user_name","")
+def addBranch(restTree, subTree):
+    global diseaseDict
+    if subTree is None:
+        return  None
+    elif restTree is None:
+        return subTree
+    else:
+        for element in subTree:
+            if element in restTree:
+                print("found element *" + str(element) + "* in subTree")
+                #print(restTree[element])
+                print(subTree[element])
+                restTree = {element:addBranch(restTree[element], subTree[element])}
+                return restTree
+            else:
+                if isinstance(restTree, tuple):
+                    return tuple(subTree) + restTree
+                else:
+                    return (subTree, restTree)
+
+def testing():
+    print("HERE WE GO")
+    testList1 = ["lets", "test", "that"]
+    testSubTree1 = (makeEntry(testList1, "1"))
+    print(testSubTree1)
+    testList2 = ["lets", "test"]
+    testSubTree2 = (makeEntry(testList2, "2"))
+    print(testSubTree2)
+    testList3 = ["lets", "test", "this"]
+    testSubTree3 = (makeEntry(testList3, "3"))
+    print(testSubTree3)
+    mainTree = addBranch(testSubTree1, testSubTree2)
+    print(mainTree)
+    #mainTree = addBranch(testSubTree3, mainTree)
+    #print(mainTree)
+
+#right now i am not covering more than one identifier per node
+def appendToMainTree(mainTree, subTree):
+    global diseaseDict
+    if subTree is None:
+        return None
+    if mainTree is None:
+        return subTree
+    else:
+        for subTreeElement in subTree:
+            print("iterating subTree elements")
+            if subTreeElement in mainTree:
+                print("found subTree in main Tree")
+                for element in mainTree:
+                    if isinstance(element, dict):
+                        print("we found a dict")
+                        return appendToMainTree(element, subTreeElement)
+                    else:
+                        #TODO this right here is not very type save
+                         mainTree[subTreeElement] = (element, appendToMainTree(None, subTreeElement))
+                         break
+            else:
+                mainTree[subTreeElement] = subTree
+                return mainTree
+            
+
+def findSimiliarity(subTree, tree):
+  result = []
+  subTreeTemp = subTree.copy()
+  while tree is not None and len(subTreeTemp)>0:
+    tree = tree.get(subTreeTemp[0])
+    if tree is not None:
+      result.append(subTreeTemp[0])
+    subTreeTemp.pop(0)
+  return result
+
 
 def matchNames(word):
     global foundExact, matchFound, foundSynonym
@@ -32,17 +114,17 @@ def matchNames(word):
             foundExact +=1
             diseaseIden = diseaseDict[word]
         elif isinstance(diseaseDict[word], int):
-            print(searchQuery[0:diseaseDict[word]])
+            #print(searchQuery[0:diseaseDict[word]])
             word = " ".join(searchQuery[0:diseaseDict[word]+1])
             #word = set(searchQuery[0:diseaseDict[word]+1])
             #for element in synonymDict:
-            if word not in tabooDict:
-                if word in synonymDict:
+            #if word not in tabooDict:
+            if word in synonymDict:
                     #if len(word.difference(set(element.split()))) <= len(element)-2:
-                    diseaseIden = synonymDict[word]
-                    foundSynonym +=1
-                else:
-                    tabooDict[word] = True
+                diseaseIden = synonymDict[word]
+                foundSynonym +=1
+                #else:
+                    #tabooDict[word] = True
     if diseaseIden is not None:
         print('AHA. found a match in the diseaseDict: found ' + str(word) + ' in disease ' + str(diseaseIden))
         matchFound = True
@@ -55,83 +137,105 @@ def buildResults(drugIdentifier, diseaseIdentifier, indication, diseaseName):
     return 0
 
 
-#hier der Aufruf einer query und wie man die Ergebnisse durchläuft.
+def loadDiseases():
+    #hier der Aufruf einer query und wie man die Ergebnisse durchläuft.
+    global overwrittenEntries
+    query = 'MATCH (n:Disease) RETURN n'
+    results = g.run(query)
 
-query = 'MATCH (n:Disease) RETURN n'
-results = g.run(query)
-
-
-# load in disease names first and the match their names while you load in drug indications
-
-for result, in results:
-    name = result['name']
-    identifier = result['identifier']
-    synonyms = result['synonyms']
-    if synonyms is None:
-        synonymWords = name
-        namesAndSynonyms = name
-    else:
-        synonymWords = synonyms
-        namesAndSynonyms = synonyms[:]
-        namesAndSynonyms.append(name)
-    diseaseInden[identifier] = namesAndSynonyms
-    if synonyms is None:
-        diseaseDict[name] = identifier
-    else:
+    for result, in results:
+        name = result['name']
+        identifier = result['identifier']
+        synonyms = result['synonyms']
+        if synonyms is None:
+            synonyms = name
+            synonymWords = name
+            namesAndSynonyms = name
+        else:
+            synonymWords = synonyms
+            namesAndSynonyms = synonyms[:]
+            namesAndSynonyms.append(name)
+        diseaseInden[identifier] = namesAndSynonyms
+        # this is commented out, since its an old approach
+        #if synonyms is None:
+        #    diseaseDict[name.lower()] = identifier
+        #else:
+        #    diseaseDict[name.lower()] = identifier
         for element in synonyms:
-            diseaseDict[name] = identifier
             element.lower()
             synonymWords = element.split()
-            if len(synonymWords) == 1:
-                diseaseDict[synonymWords[0]] = identifier
+            subTree = makeEntry(synonymWords, identifier)
+            if synonymWords[0] in diseaseDict:
+                diseaseDict[synonymWords[0]] = addBranch(diseaseDict, subTree)
             else:
-                diseaseDict[synonymWords[0]] = int(len(synonymWords))
-                synonymWords = " ".join(synonymWords)
-                synonymDict[synonymWords] = identifier
+                diseaseDict[synonymWords[0]] = subTree
+            #appendToMainTree(subTree)
+            #findSimiliarity(subTree, diseaseDict)
+            #if len(synonymWords) == 1:
+            #    diseaseDict[synonymWords[0]] = identifier
+            #else:
+             #   diseaseDict[synonymWords[0]] = int(len(synonymWords))
+              #  synonymWords = " ".join(synonymWords)
+               # synonymDict[synonymWords] = identifier
 
 
 
-query = 'MATCH (n:Compound) WHERE EXISTS(n.indication) RETURN n'
-results = g.run(query)
+def loadDrugs():
+    query = 'MATCH (n:Compound) WHERE EXISTS(n.indication) RETURN n LIMIT 500'
+    results = g.run(query)
+
+    for result, in results:
+        matchFound = False
+        name = result['name']
+        identifier = result['identifier']
+        indication = result['indication'].lower().translate(str.maketrans('', '', string.punctuation))
+
+        splitIndication = indication.split()
+        wordCount = 0
+        for element in splitIndication:
+            wordCount += 1
+            searchQuery = splitIndication[:wordCount]
+            #print('looking for word "' + element + '" in list of diseases')
+            diseaseIdentifier = matchNames(element)
+            if diseaseIdentifier is not None:
+                buildResults(identifier, diseaseIdentifier, indication, diseaseInden[diseaseIdentifier])
+        if not matchFound:
+            lonelyDrug.append((identifier, indication))
+            #drugDict[identifier] = indication.split()
+
+def writeResults():
+    with open('finalList.csv', 'w', newline='', encoding="utf-8") as csvfile:
+        nameWriter = csv.writer(csvfile, delimiter=' ',  quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for element in matchList:
+            nameWriter.writerow([element])
+
+    with open('lonelyDrugs.csv', 'w', newline='', encoding="utf-8") as csvfile:
+        nameWriter = csv.writer(csvfile, delimiter=' ',  quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for element in lonelyDrug:
+            nameWriter.writerow([element])
 
 
-for result, in results:
-    matchFound = False
-    name = result['name']
-    identifier = result['identifier']
-    indication = result['indication'].lower().translate(str.maketrans('', '', string.punctuation))
 
-    splitIndication = indication.split()
-    wordCount = 0
-    for element in splitIndication:
-        wordCount += 1
-        searchQuery = splitIndication[:wordCount]
-        #print('looking for word "' + element + '" in list of diseases')
-        diseaseIdentifier = matchNames(element)
-        if diseaseIdentifier is not None:
-            buildResults(identifier, diseaseIdentifier, indication, diseaseInden[diseaseIdentifier])
-    if not matchFound:
-        lonelyDrug.append((identifier, indication))
-        #drugDict[identifier] = indication.split()
-        #search here
+def main():
+    loadDiseases()
+    #loadDrugs()
+    # print(diseaseDict)
+    # print(synonymDict)
+    print(foundExact)
+    print(foundSynonym)
+    print("overwritten Entries : " + str(overwrittenEntries))
+    print(tabooDict)
+    writeResults()
+    testing()
 
 
-with open('finalList.csv', 'w', newline='', encoding="utf-8") as csvfile:
-    nameWriter = csv.writer(csvfile, delimiter=' ',  quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for element in matchList:
-        nameWriter.writerow([element])
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
 
-with open('lonelyDrugs.csv', 'w', newline='', encoding="utf-8") as csvfile:
-    nameWriter = csv.writer(csvfile, delimiter=' ',  quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for element in lonelyDrug:
-        nameWriter.writerow([element])
-
-
-#print(diseaseDict)
-#print(synonymDict)
-print(foundExact)
-print(foundSynonym)
 
 #TODO
+#WORK ON TABOODICT, THAT MAKES NO SENSE LIKE THAT
 #note down drugs with no match with indication
 #implement better search
+#ignore diesease knoteen mit it 00000001
